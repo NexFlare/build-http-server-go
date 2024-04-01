4c7e4fb6ed1b30abe65a15b340d5447adbd91277
Nexflare <harshmagarwal@outlook.com>
2024-04-01 15:19:20 -0400

    Stage 3 : Respond with 404 added

--- app/server.go
+++ app/server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
+
+type void struct{}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -27,10 +30,27 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+	}
+	req := string(buffer[:])
+	reqLines := strings.Split(req, "\r\n")
+	startLine := reqLines[0]
+	path := strings.Split(startLine, " ")[1]
+	set := make(map[string]void)
+	set["/"] = void{}
+	if _, ok:= set[path]; ok {
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	if err != nil {
 		fmt.Println("Error while writing: ", err.Error())
 		os.Exit(1)
 	}
 }
+
+// func
